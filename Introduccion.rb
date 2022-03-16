8e3643f9209943a89d17d27238c8c01efd3def07
Aaron776 <aronortiz90@yahoo.com>
2022-03-16 18:11:45 -0500

    Actualizar

---
+++ Introduccion.rb
@@ -0,0 +1,12 @@
+#Ruby es un lenguaje de programacion multiplataforma orientado a objetos, es muy amigable en lo que tiene que ver con la forma de codificacion parecido al python, es un lenguaje muy sencillo
+
+# Para imprimir en pantalla se usa la notacion puts y no se necesita poner ; para finalizar esa linea de codigo ya que este lenguaje es de facil tipado, es muy parecido al print de python o el document.write() del javasript
+puts "Hola Mundo" 
+puts "Mi nombre es Aaron Ortiz"
+
+# Para ejecutar un archibo con extencion ruby debemos hacerlo mediante el uso de la consola cmd usando la palabra ruby seguido de la ruta del archivo que queremos ejecutar
+
+
+=begin
+para realizar comentarios multilinea uso las expresiones =begin y =end y entre ellos escribo lo que quiero comentar
+=end
